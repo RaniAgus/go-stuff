9265edb5f9ba180cb7d2925c12a844e75f97a1e7
Agustin Ranieri <aguseranieri@gmail.com>
2023-09-18 16:53:18 -0300

    importing packages from your module

--- hello.go
+++ hello.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/RaniAgus/hello/morestrings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
